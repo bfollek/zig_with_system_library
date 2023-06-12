7b090b214c77d0d305d2ffd2463646390e8ffae4
Bob Follek <bfollek@gmail.com>
2023-06-12 17:02:08 -0400

    WIP

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,9 @@
 const std = @import("std");
-const c = @cImport(@cInclude("editline/readline.h"));
+const rl = @cImport(@cInclude("editline/readline.h"));
 
 pub fn main() !void {
-    std.debug.print("in main\n", .{});
-    //_ = c.add_history("foo");
-
     while (true) {
-        const input = c.readline("Enter something (CTRL/C to stop)> ");
-        _ = c.add_history(input);
+        const input = rl.readline("Enter something (CTRL/C to stop)> ");
+        _ = rl.add_history(input);
     }
-
-    //         printf("No you're a %s\n", input);
-    //         free(input);
-    //var i: i64 = c.add_in_c(3, 4);
-    //std.debug.print("i == {d}\n", .{i});
-}
-
-test "simple test" {
-    std.debug.print("in simple test\n", .{});
-    //try std.testing.expectEqual(@as(i64, 7), c.add_in_c(3, 4));
 }
