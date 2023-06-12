328824eb7901e4ff2ed3640044d27d12b33ea344
Bob Follek <bfollek@gmail.com>
2023-06-12 17:21:09 -0400

    WIP

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,16 @@
 const std = @import("std");
-const rl = @cImport(@cInclude("editline/readline.h"));
+const c_el = @cImport(@cInclude("editline/readline.h"));
+const c_std = @cImport(@cInclude("stdlib.h"));
 
 pub fn main() !void {
     while (true) {
-        const input = rl.readline("Enter something (CTRL/C to stop)> ");
-        _ = rl.add_history(input);
+        const input: [*c]u8 = c_el.readline("Enter something (CTRL/C to stop)> ");
+        say_hello(input);
+        _ = c_el.add_history(input);
+        c_std.free(input);
     }
+}
+
+fn say_hello(input: [*c]u8) void {
+    std.debug.print("Hello, {s}!\n", .{input});
 }
